23e6adf0f5c052434975ea40b934a4a113553a5f
AndrewMos <moskovecandrey@gmail.com>
2018-03-19 17:16:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const gameName = "MoskovStudioGame";
 
 const queries = {};
 
-bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "This bot implements a T-Rex jumping game. Say /game if you want to play."));
+bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "This bot is game bot. Say /game if you want to play."));
 bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
 bot.on("callback_query", function (query) {
     if (query.game_short_name !== gameName) {
